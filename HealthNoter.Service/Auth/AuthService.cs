fce655dfd373a8c2b1330de9f0db3a7baa5dbe08
artemkiruhin <temkaunik@gmail.com>
2024-12-12 19:12:22 +0300

    Auth service upd: hashing

--- HealthNoter.Service/Auth/AuthService.cs
+++ HealthNoter.Service/Auth/AuthService.cs
@@ -1,6 +1,7 @@
 using HealthNoter.Core.Entities;
 using HealthNoter.DataAccess.Postgresql.Repositories.Base;
 using HealthNoter.Service.Entity.Base;
+using HealthNoter.Service.Hash;
 using HealthNoter.Service.JWT;
 
 namespace HealthNoter.Service.Auth;
@@ -10,17 +11,19 @@ public class AuthService : IAuthService
     private readonly IUserService _userService;
     private readonly IUserRepository _userRepository;
     private readonly IJwtService _jwtService;
+    private readonly IHashService _hashService;
 
-    public AuthService(IUserService userService, IUserRepository userRepository, IJwtService jwtService)
+    public AuthService(IUserService userService, IUserRepository userRepository, IJwtService jwtService, IHashService hashService)
     {
         _userService = userService;
         _userRepository = userRepository;
         _jwtService = jwtService;
+        _hashService = hashService;
     }
     
     public async Task<string> Login(string username, string password, CancellationToken ct)
     {
-        var user = await _userRepository.GetByUsernameAndPassword(username, password, ct);
+        var user = await _userRepository.GetByUsernameAndPassword(username, _hashService.ComputeHash(password), ct);
         if (user == null) throw new KeyNotFoundException();
 
         var token = _jwtService.GenerateToken(user.Id);
@@ -29,9 +32,9 @@ public class AuthService : IAuthService
 
     public async Task<bool> Register(string username, string password, CancellationToken ct)
     {
-        var user = await _userRepository.GetByUsernameAndPassword(username, password, ct);
+        var user = await _userRepository.GetByUsernameAndPassword(username, _hashService.ComputeHash(password), ct);
         if (user != null) throw new ArgumentException();
 
-        return await _userService.Add(username, password, ct);
+        return await _userService.Add(username, _hashService.ComputeHash(password), ct);
     }
 }
