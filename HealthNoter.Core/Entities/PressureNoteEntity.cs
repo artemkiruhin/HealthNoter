08a0966dfe650fc731b6079ccbe1226f97a627f3
artemkiruhin <temkaunik@gmail.com>
2024-12-12 16:36:19 +0300

    Entities created

---
+++ HealthNoter.Core/Entities/PressureNoteEntity.cs
@@ -0,0 +1,28 @@
+﻿namespace HealthNoter.Core.Entities;
+
+public class PressureNoteEntity
+{
+    public static PressureNoteEntity Create(int sys, int dia, int pulse, Guid userId)
+    {
+        if (sys < 0 || dia < 0 || pulse < 0)
+            throw new ArgumentOutOfRangeException("Кардио параметры не могут быть неположительными");
+
+        return new PressureNoteEntity()
+        {
+            Id = Guid.NewGuid(),
+            Sys = sys,
+            Dia = dia,
+            Pulse = pulse,
+            CreatedAt = DateTime.UtcNow,
+            UserId = userId
+        };
+    }
+
+    public Guid Id { get; set; }
+    public int Sys { get; set; }
+    public int Dia { get; set; }
+    public int Pulse { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid UserId { get; set; }
+    public virtual UserEntity User { get; set; } = null!;
+}
---
+++ HealthNoter.Core/Entities/UserEntity.cs
@@ -0,0 +1,31 @@
+﻿namespace HealthNoter.Core.Entities;
+
+public class UserEntity
+{
+    private const int UsernameMinSize = 3;
+    private const int UsernameMaxSize = 3;
+
+
+    public static UserEntity Create(string username, string password)
+    {
+        if (username.Length is < UsernameMinSize or > UsernameMaxSize)
+            throw new ArgumentException(
+                $"Имя пользователя должно быть в пределах от {UsernameMinSize} до {UsernameMaxSize}");
+
+        return new UserEntity()
+        {
+            Id = Guid.NewGuid(),
+            Username = username,
+            Password = password,
+            RegisteredAt = DateTime.UtcNow
+        };
+    }
+
+    public Guid Id { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public DateTime RegisteredAt { get; set; }
+    public virtual ICollection<PressureNoteEntity> PressureNotes { get; set; } = new List<PressureNoteEntity>();
+    
+    
+}
