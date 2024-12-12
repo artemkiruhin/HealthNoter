64390c9a4e9135b6a2fb5135fb7fcdadde646b02
artemkiruhin <temkaunik@gmail.com>
2024-12-12 18:07:22 +0300

    Auth service base created

---
+++ HealthNoter.Service/Auth/AuthService.cs
@@ -0,0 +1,14 @@
+namespace HealthNoter.Service.Auth;
+
+public class AuthService : IAuthService
+{
+    public async Task<string> Login(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> Register(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ HealthNoter.Service/Auth/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace HealthNoter.Service.Auth;
+
+public interface IAuthService
+{
+    Task<string> Login(string username, string password);
+    Task<bool> Register(string username, string password);
+}
